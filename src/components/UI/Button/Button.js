1e3e7b24be5b886fcfb7a8b0d0bd563e80f07a50
daphneruth <daphneruth304@gmail.com>
2022-06-04 16:15:01 +0300

    reinstalled modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,9 @@
 
 
-import styled from 'styled-omponents';
+import styled from 'styled-components';
+
 
 const Button = styled.button`
- 
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -13,7 +13,7 @@ const Button = styled.button`
   cursor: pointer;
 
 
- & : focus{
+ &:focus {
   outline: none;
 }
 
